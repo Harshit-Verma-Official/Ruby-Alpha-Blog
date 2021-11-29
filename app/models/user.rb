41a0c9bfa2dcf67e988c6c8a94b8362a82903770
Harshit Verma <harshit.verma@unthinkable.co>
2021-11-29 13:30:04 +0530

    Add users create action.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
     has_many :articles
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :username, uniqueness: {case_sensitive: false}, 
@@ -6,4 +7,6 @@ class User < ApplicationRecord
     validates :email, uniqueness: {case_sensitive: false}, 
                         presence: true, length: {maximum: 105},
                         format: { with: VALID_EMAIL_REGEX }
+    
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_104236) do
+ActiveRecord::Schema.define(version: 2021_11_29_052204) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_11_26_104236) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
